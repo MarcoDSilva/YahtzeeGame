ced4e46e6e20a74386c9067026d69a9dc80e03f3
MarcoDiSilva <38049196+MarcoDSilva@users.noreply.github.cm>
2019-07-22 20:15:00 +0100

    update

--- Yahtzee/Yahtzee/Form1.cs
+++ Yahtzee/Yahtzee/Form1.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Yahtzee
 {
@@ -63,18 +64,46 @@ namespace Yahtzee
         public void higherScoreChecker()
         {
             //verifies the number of dices from Aces to Sixes score and multiplies it by the respective number if more than one dice
-            lbl_aces.Text = Convert.ToString(SimpleDices(1));
-            lbl_twos.Text = Convert.ToString(SimpleDices(2) * 2);
-            lbl_threes.Text = Convert.ToString(SimpleDices(3) * 3);
-            lbl_fours.Text = Convert.ToString(SimpleDices(4) * 4);
-            lbl_fives.Text = Convert.ToString(SimpleDices(5) * 5);
-            lbl_sixes.Text = Convert.ToString(SimpleDices(6) * 6);
+            //if the background is yellow, stops the verification, which means the dice was locked by the player
+            if(lbl_aces.BackColor != Color.Yellow)
+            {
+                lbl_aces.Text = Convert.ToString(SimpleDices(1));
+
+            }
+
+            if(lbl_twos.BackColor != Color.Yellow)
+            {
+                lbl_twos.Text = Convert.ToString(SimpleDices(2) * 2);
+            }
+
+            if(lbl_threes.BackColor != Color.Yellow)
+            {
+                lbl_threes.Text = Convert.ToString(SimpleDices(3) * 3);
+            }
+
+            if (lbl_fours.BackColor != Color.Yellow)
+            {
+                lbl_fours.Text = Convert.ToString(SimpleDices(4) * 4);
+            }
+
+            if (lbl_fives.BackColor != Color.Yellow)
+            {
+                lbl_fives.Text = Convert.ToString(SimpleDices(5) * 5);
+            }
+
+            if (lbl_sixes.BackColor != Color.Yellow)
+            {
+                lbl_sixes.Text = Convert.ToString(SimpleDices(6) * 6);
+            }
+            
         }
 
         //method that verifies the bottom part
         //trios, four of a kind, sequences, fh, yahtzee, chances
         public void lowerScoreChecker()
         {
+            //calls the method that counts the number of dices corresponding to searched dice
+            //dice1 gets the total of number 1 dices, dice2 the number of dices number 2 , etc...
             int dice1 = SimpleDices(1);
             int dice2 = SimpleDices(2);
             int dice3 = SimpleDices(3);
@@ -82,6 +111,8 @@ namespace Yahtzee
             int dice5 = SimpleDices(5);
             int dice6 = SimpleDices(6);
 
+            //gets the value of the actual rolled dices
+            //to use for calculations
             int dice1_val = Convert.ToInt32(lbl_displayDice1.Text);
             int dice2_val = Convert.ToInt32(lbl_displayDice2.Text);
             int dice3_val = Convert.ToInt32(lbl_displayDice3.Text);
@@ -96,32 +127,132 @@ namespace Yahtzee
             {
                 lbl_score3Kind.Text = Convert.ToString(dice1_val + dice2_val + dice3_val + dice4_val + dice5_val);
             }
+            else
+            {
+                lbl_score3Kind.Text = "0";
+            }
 
             //four of a kind
             if (dice1 >= 4 || dice2 >= 4 || dice3 >= 4 || dice4 >= 4 || dice5 >= 4 || dice6 >= 4)
             {
                 lbl_4KindScore.Text = Convert.ToString(dice1_val + dice2_val + dice3_val + dice4_val + dice5_val);
-            }            
+            }
+            else
+            {
+                lbl_4KindScore.Text = "0";
+            }
+            
+            //============full house
+            //TODO: FIX
+            if (dice1 >= 3)
+            {
+               if(dice2 == 2 || dice3 == 2 || dice4 == 2 || dice5 == 2 || dice6 == 2)
+                {
+                    lbl_scoreFH.Text = "25";
+                }
+                else
+                {
+                    lbl_scoreFH.Text = "0";
+                }
+            }
+          
+            if (dice2 >= 3)
+            {
+                if (dice1 == 2 || dice3 == 2 || dice4 == 2 || dice5 == 2 || dice6 == 2)
+                {
+                    lbl_scoreFH.Text = "25";
+                }
+                else
+                {
+                    lbl_scoreFH.Text = "0";
+                }
+            }
+           
+
+            if (dice3 >= 3)
+            {
+                if (dice1 == 2 || dice2 == 2 || dice4 == 2 || dice5 == 2 || dice6 == 2)
+                {
+                    lbl_scoreFH.Text = "25";
+                }
+                else
+                {
+                    lbl_scoreFH.Text = "0";
+                }
+            }
+           
+
+            if (dice4 >= 3)
+            {
+                if (dice1 == 2 || dice2 == 2 || dice3 == 2 || dice5 == 2 || dice6 == 2)
+                {
+                    lbl_scoreFH.Text = "25"; ;
+                }
+                else
+                {
+                    lbl_scoreFH.Text = "0";
+                }
+            }
+           
+
+            if (dice5 >= 3)
+            {
+                if (dice1 == 2 || dice2 == 2 || dice3 == 2 || dice4 == 2 || dice6 == 2)
+                {
+                    lbl_scoreFH.Text = "25";
+                }
+                else
+                {
+                    lbl_scoreFH.Text = "0";
+                }
+            }
+           
+
+            if (dice6 >= 3)
+            {
+                if (dice1 == 2 || dice2 == 2 || dice3 == 2 || dice4 == 2 || dice5 == 2)
+                {
+                    lbl_scoreFH.Text = "25";
+                }
+                else
+                {
+                    lbl_scoreFH.Text = "0";
+                }
+            }
+            
+            //=============== full house end
 
             //lower straights
-            if ((dice1 == 1 && dice2 == 2 && dice3 == 3 && dice4 == 4) || (dice1 == 2 && dice2 == 3 && dice3 == 4 && dice4 == 5) ||
-                    (dice1 == 3 && dice2 == 4 && dice3 == 5 && dice4 == 6) || (dice2 == 1 && dice3 == 2 && dice4 == 3 && dice5 == 4) ||
-                       (dice2 == 2 && dice3 == 3 && dice4 == 4 && dice5 == 5) || (dice2 == 3 && dice3 == 4 && dice4 == 5 && dice5 == 6))
+            if ((dice1_val == 1 && dice2_val == 2 && dice3_val == 3 && dice4_val == 4) || (dice1_val == 2 && dice2_val == 3 && dice3_val == 4 && dice4_val == 5) ||
+                    (dice1_val == 3 && dice2_val == 4 && dice3_val == 5 && dice4_val == 6) || (dice2_val == 1 && dice3_val == 2 && dice4_val == 3 && dice5_val == 4) ||
+                       (dice2_val == 2 && dice3_val == 3 && dice4_val == 4 && dice5_val == 5) || (dice2_val == 3 && dice3_val == 4 && dice4_val == 5 && dice5_val == 6))
             {
-                lbl_scoreLStraight.Text = Convert.ToString(30);
+                lbl_scoreLStraight.Text = "30";
+            }
+            else
+            {
+                lbl_scoreLStraight.Text = "0";
             }
 
             //large straight
-            if ((dice1 == 1 && dice2 == 2 && dice3 == 3 && dice4 == 4 && dice5 == 5) ||
-                    dice1 == 2 && dice2 == 3 && dice3 == 4 && dice4 == 5 && dice5 == 6)
+            if ((dice1_val == 1 && dice2_val == 2 && dice3_val == 3 && dice4_val == 4 && dice5_val == 5) ||
+                    dice1_val == 2 && dice2_val == 3 && dice3_val == 4 && dice4_val == 5 && dice5_val == 6)
             {
-                lbl_scoreHStraight.Text = Convert.ToString(40);
+                lbl_scoreHStraight.Text = "40";
+            }
+            else
+            {
+                lbl_scoreHStraight.Text = "0";
             }
 
             //yahtzee
-            if (dice1 >= 5 || dice2 >= 5 || dice3 >= 5 || dice4 >= 5 || dice5 >= 5 || dice6 >= 5)
+            if (dice1 == 5 || dice2 == 5 || dice3 == 5 || dice4 == 5 || dice5 == 5 || dice6 == 5)
             {
-                lbl_scoreYat.Text = Convert.ToString(50);
+                lbl_scoreYat.Text = "50";
+            }
+            else
+            {
+                lbl_scoreYat.Text = "0";
             }
 
             //int conversions for final score
@@ -148,32 +279,48 @@ namespace Yahtzee
 
         // ----------------- LABEL CLICK EVENTS -----------------
       
-        private void label_click_aces(object sender, EventArgs e)
+        public void label_click_aces(object sender, EventArgs e)
         {
             Label click_aces = sender as Label;
 
-            if(click_aces != null)
+            if(click_aces.Enabled)
             {
-                MessageBox.Show("ACES LOCKED!");
+                click_aces.Enabled = false;
             }
+
+            if(!click_aces.Enabled)
+            {
+                lbl_aces.BackColor = Color.Yellow;
+            }
+            
         }
         private void label_click_deuces(object sender, EventArgs e)
         {
             Label click_deuces = sender as Label;
 
-            if (click_deuces != null)
+            if (click_deuces.Enabled)
             {
-                MessageBox.Show("DEUCES LOCKED!");
+                click_deuces.Enabled = false;
+            }
+
+            if (!click_deuces.Enabled)
+            {
+                click_deuces.BackColor = Color.Yellow;
             }
         }
 
         private void label_click_threes(object sender, EventArgs e)
         {
             Label click_three = sender as Label;
-
-            if (click_three != null)
+           
+            if (click_three.Enabled)
             {
-                MessageBox.Show("THRES LOCKED!");
+                click_three.Enabled = false;
+            }
+
+            if (!click_three.Enabled)
+            {
+                click_three.BackColor = Color.Yellow;
             }
         }
 
@@ -181,9 +328,14 @@ namespace Yahtzee
         {
             Label click_fours = sender as Label;
 
-            if (click_fours != null)
+            if (click_fours.Enabled)
             {
-                MessageBox.Show("FOURS LOCKED!");
+                click_fours.Enabled = false;
+            }
+
+            if (!click_fours.Enabled)
+            {
+                click_fours.BackColor = Color.Yellow;
             }
         }
 
@@ -191,9 +343,14 @@ namespace Yahtzee
         {
             Label click_fives = sender as Label;
 
-            if (click_fives != null)
+            if (click_fives.Enabled)
             {
-                MessageBox.Show("FIVES LOCKED!");
+                click_fives.Enabled = false;
+            }
+
+            if (!click_fives.Enabled)
+            {
+                click_fives.BackColor = Color.Yellow;
             }
         }
 
@@ -201,10 +358,16 @@ namespace Yahtzee
         {
             Label click_sixes = sender as Label;
 
-            if (click_sixes != null)
+            if (click_sixes.Enabled)
             {
-                MessageBox.Show("SIXES LOCKED!");
+                click_sixes.Enabled = false;
             }
+
+            if (!click_sixes.Enabled)
+            {
+                click_sixes.BackColor = Color.Yellow;
+            }
+
         }
 
         private void label_click_3Kind(object sender, EventArgs e)
